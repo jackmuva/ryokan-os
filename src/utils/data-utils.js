5d829c8fffb2ab7a950298e6c56230b7cefba8ad
jackmuva <jackmu@umich.edu>
2025-05-04 16:13:46 -0400

    logic to handling when characters are defeated

---
+++ src/utils/data-utils.js
@@ -0,0 +1,13 @@
+
+import { DATA_ASSET_KEYS } from "../assets/asset-keys.js";
+export class DataUtils {
+	/** 
+	 * @param {Phaser.Scene} scene
+	 * @param {Number} attackId
+	 */
+	static getMonsterAttack(scene, attackId) {
+		/** @type {import("../types/typedef.js").Attack[]}*/
+		const data = scene.cache.json.get(DATA_ASSET_KEYS.ATTACKS);
+		return data.find((attack) => attack.id === attackId);
+	}
+}
